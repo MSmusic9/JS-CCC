128b30ed2bfffee2c2df6ad84a57e560867f5b99
MSmusic9 <121720254+MSmusic9@users.noreply.github.com>
2023-03-06 16:21:55 +0300

    Update Cpl.js

--- Cpl.js
+++ Cpl.js
@@ -1,2 +1,23 @@
-class Cpl:
-  
+import kaboom from "kaboom"
+
+class Cpl {
+  this.pause = false
+  constructor(width,height) {}
+  function pause() {
+    pause = true
+  }
+  function startAni(width,height,fps,paths) {
+    while () {
+      for (var i = 0;i == this.paths.length;i++) {
+        loadSprite(this.paths[i],this.paths[i]) 
+        setTimeout(() => {
+          kaboom({
+            width: this.width, 
+            height: this.height, 
+            background: [sprite(this.paths[i]),scale(this.width,this.height)]
+          }) 
+        }, 1000/this.fps) 
+      }
+    }
+  }
+}
